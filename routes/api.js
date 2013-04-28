563ee8063d15ae5e1f5b6b1a242e1bed9cb8f101
Shaun <shaunr88@gmail.com>
2013-04-28 01:37:56 -0400

    Commit so i can make a branch for some refactoring. Fixed some post stuff

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,27 @@ var Track = require('../models/track.js');
 var NoteGroup = require('../models/noteGroup')
 var db = require('../db/db')
 
+var db_callback = function(success_callback, err_callback){
+  return function(err, body) {
+    if(!err) {
+        console.log("Successful db op: " + body);
+        success_callback();
+    } else {
+        console.log("Failed db op. Body: " + body + " Error: " + err);
+        err_callback();  
+    }
+  };
+};
+
+var success = function() {
+
+}
+
+var error = function() {
+
+}
+
+
 /**
  * Api Crud operations
  **/
@@ -58,39 +79,39 @@ module.exports = function (app) {
     });
 
     app.post('/composition', function(req, res, next) {
-        var comp_db = nano.db.use('composition');	
         var comp = Composition.createFromObject(req.body);
         if(Composition.validate(comp)) {
-            comp_db.insert(comp);
-            res.json({"OK": true});
+            db.compositions.insert(comp, null, 
+                db_callback(
+                    function(){ res.json({"OK":false}); },
+                    function(){ res.send(500, "Could not add: " + req.body); }
+                ));
         } else {
             res.json({"OK": false});
         }
     });
-
+    
     app.post('/track', function(req, res, next) {
-        var track_db = nano.db.use('track');	
-	var track = Track.createFromObject(req.body);
-	if(Track.validate(track)) {
-	    track_db.insert(track);
-	    res.json({"OK": true});
-	} else {
-	    res.json({"OK": false});
-	}
+	    var track = Track.createFromObject(req.body);
+	    if(Track.validate(track)) {
+            db.tracks.insert(track);
+            res.json({"OK": true});
+	    } else {
+	        res.json({"OK": false});
+	    }
     });
-
+    
     app.post('/note_group', function(req, res, next) {
-        var note_group_db = nano.db.use('note_group');	
-	var note_group = Note_Group.createFromObject(req.body);
-	if(Note_Group.validate(note_group)) {
-	    note_group_db.insert(note_group);
-	    res.json({"OK": true});
-	} else {
-	    res.json({"OK": false});
-	}
+	    var noteGroup = NoteGroup.createFromObject(req.body);
+	    if(NoteGroup.validate(noteGroup)) {
+            db.noteGroups.insert(noteGroup);
+	        res.json({"OK": true});
+	    } else {
+	        res.json({"OK": false});
+	    }
     });
-
-
+    
+    
     /**  GET, POST, PUT, DELETE **/
-
+    
 }
