2e56e9d97d3dc7a0dcb23bdc63adf8cf0d1c7440
root <root@john-android-dev.(none)>
2013-04-27 20:09:52 -0400

    Adding list functionality for compositions.

--- routes/api.js
+++ routes/api.js
@@ -7,10 +7,19 @@ var nano = require('nano')('http://localhost:5984');
  * Api Crud operations
  **/
 module.exports = function (app) {
+	app.get('/composition', function(req, res, next) {
+		var comp_db = nano.db.use('composition');
+		comp_db.list({ revs_info: false, value: false }, function(err, body) {
+			if (!err) {
+				res.json(body.rows);
+			}
+		});
+	});
+	
     app.get('/composition/:id', function(req, res, next) {
         var comp_id = req.param('id');
         var comp_db = nano.db.use('composition');
-        comp_db.get(comp_id, { revs_info: true }, function(err, body) {
+        comp_db.get(comp_id, { revs_info: false }, function(err, body) {
             if (!err)
                 res.json(body)
         });
@@ -19,7 +28,7 @@ module.exports = function (app) {
     app.get('/track/:id', function(req, res, next) {
         var id = req.param('id');
         var db = nano.db.use('tracks');
-        db.get(id, { revs_info: true }, function(err, body) {
+        db.get(id, { revs_info: false }, function(err, body) {
             if (!err)
                 res.json(body)
         });
@@ -28,7 +37,7 @@ module.exports = function (app) {
     app.get('/note_group/:id', function(req, res, next) {
         var id = req.param('id');
         var db = nano.db.use('note_groups');
-        db.get(id, { revs_info: true }, function(err, body) {
+        db.get(id, { revs_info: false }, function(err, body) {
             if (!err)
                 res.json(body)
         });
