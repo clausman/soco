5a7d7374bbb5cea9bf26c63ea4d39d4ab2f54afe
John Bufe <jpbufe3@gmail.com>
2013-04-28 11:32:45 -0400

    Adding the rest of the PUT methods.

--- routes/apiPut.js
+++ routes/apiPut.js
@@ -38,8 +38,64 @@ module.exports = function (app) {
                 res.json({"OK": false});
         });
     });
+    
+    app.put('/track/:id', function(req, res, next) {
+        var track_db = db.tracks;
+        var track_id = req.param('id');
+        var track = {};
+        track_db.get(track_id, { revs_info: false }, function(err, body) {
+            if (!err) {
+                track = Track.createFromObject(body);
 
+		    	var instrument = req.body.instrument;
+		    	if(!(instrument === undefined || instrument === null)) track.instrument = instrument;
+		    	var note_groups = req.body.note_groups;
+		    	if(!(note_groups === undefined || note_groups === null)) track.note_groups = note_groups;
+		    	var name = req.body.name;
+		    	if(!(name === undefined || name === null)) track.name = name;
+		        if(Track.validate(track)) {
+		            track_db.insert(track, function(err, resp) {
+		            	if(!err)
+		            		res.json({"OK": true});
+		            	else
+		            		res.json({"OK": false});
+		            });
+		        }
+		       	else
+		        	res.json({"OK": false});
+            } else
+                res.json({"OK": false});
+        });
+    });
 
-    /**  GET, POST, PUT, DELETE **/
+	app.put('/note_group/:id', function(req, res, next) {
+        var note_group_db = db.noteGroups;
+        var note_group_id = req.param('id');
+        var note_group = {};
+        note_group_db.get(note_group_id, { revs_info: false }, function(err, body) {
+            if (!err) {
+                note_group = NoteGroup.createFromObject(body);
+
+		    	var start = req.body.start;
+		    	if(!(start === undefined || start === null)) note_group.start = start;
+		    	var notes = req.body.notes;
+		    	if(!(notes === undefined || notes === null)) note_group.notes = notes;
+		    	var name = req.body.name;
+		    	if(!(name === undefined || name === null)) note_group.name = name;
+		        if(NoteGroup.validate(note_group)) {
+		            note_group_db.insert(note_group, function(err, resp) {
+		            	if(!err)
+		            		res.json({"OK": true});
+		            	else
+		            		res.json({"OK": false});
+		            });
+		        }
+		       	else
+		        	res.json({"OK": false});
+            } else
+                res.json({"OK": false});
+        });
+    });
+    
 
 }
