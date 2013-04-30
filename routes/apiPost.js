663bc776e4f25646da7faf3163453f91b71c1e95
Shaun <shaunr88@gmail.com>
2013-04-30 02:07:53 -0400

    Added ability to post children objects to existing objects

--- routes/apiPost.js
+++ routes/apiPost.js
@@ -6,40 +6,113 @@ var NoteGroup = require('../models/noteGroup')
 var db = require('../db/db')
 var callbacks = require('./callbacks.js');
 
+function saveChildAndAddToParent(child, childId, childModel, parentId, parentModel, callback) {
+    var findParentAndInsertChild = function(toInsert) {
+        try {
+            parentModel.find(parentId, function(parent) {
+                parent[parentModel.childArrayName].push(child);
+                if (parentModel.validate(parent)) {
+                    parentModel.save(parent, callback);
+                } else {
+                    var err = "Invalid composition";
+                    callback(err);
+                }
+            });
+        } catch (e) {
+            callback(e);
+        }
+    };
+
+    // Set childs ID now if we are choosing it
+    if(childId !== null) {
+        child._id = childId;
+    }
+
+    childModel.save(child, function(err, body) {
+        if(err) callback(err);
+        else {
+            if(childId !== null) {
+                console.log("Child id: " + childId);
+                child._rev = body.rev;
+            } else {
+                child._id = body.id;
+                child._rev = body.rev;
+            }
+            findParentAndInsertChild(child);
+        }
+    });
+}
+
+
+
 /**
  * Api Crud operations
  **/
 module.exports = function (app) {
-
+    
+    // Create a composition if one doesn't exist (auto generated id)
     app.post('/composition', function(req, res, next) {
         var comp = Composition.createFromObject(req.body);
-        var callback = callbacks.dbCallback(req, res);
-        if(Composition.validate(comp)) {
-            db.compositions.insert(comp, null, callback);
+
+        var callback = callbacks.saveCallback(req, res);
+        Composition.save(comp, callback);
+    }); 
+    
+    // Create a composition with the specified cid if it doesn't already exist
+    // Fail if it does?
+    app.post('/composition/:cid', function(req, res, next) {
+        var comp = Composition.createFromObject(req.body);
+        var cid = req.param('cid');
+        var callback = callbacks.saveCallback(req, res);
+
+        // Check for obj id and url id mismatch, error if found
+        if(typeof comp._id !== 'undefined' && comp._id !== cid ) {
+            var err = "Id conflict in submitted obj and url";
+            callback(err);
         } else {
-            res.send(400);
+            comp._id = cid;
+            Composition.save(comp, callback);
         }
     });
     
-    app.post('/track', function(req, res, next) {
+    // Create a track in composition(cid) (auto generated tid)
+    app.post('/composition/:cid/track', function(req, res, next) {
 	    var track = Track.createFromObject(req.body);
-	    var callback = callbacks.dbCallback(req, res);
-        if(Track.validate(track)) {
-            db.tracks.insert(track, null, callback);
-	    } else {
-	        res.send(400);
-	    }
+        var cid = req.param('cid');
+	    var callback = callbacks.saveCallback(req, res);
+
+        saveChildAndAddToParent(track, null, Track, cid, Composition, callback);
     });
     
-    app.post('/note_group', function(req, res, next) {
-	    var noteGroup = NoteGroup.createFromObject(req.body);
-        console.log(noteGroup);
-        var callback = callbacks.dbCallback(req, res);
-	    if(NoteGroup.validate(noteGroup)) {
-            db.noteGroups.insert(noteGroup, null, callback);
-	    } else {
-	        res.send(400);
-	    }
+    // Create a track in composition(cid) with tid
+    app.post('/composition/:cid/track/:tid', function(req, res, next) {
+	    var track = Track.createFromObject(req.body);
+        var cid = req.param('cid');
+        var tid = req.param('tid');
+	    var callback = callbacks.saveCallback(req, res);
+
+        saveChildAndAddToParent(track, tid, Track, cid, Composition, callback);
+    });
+
+    // Create a note_group in composition(cid)/track(tid) (auto generate ngid)
+    app.post('/composition/:cid/track/:tid/notegroup', function(req, res, next) {
+	    var ng = NoteGroup.createFromObject(req.body);
+        var ngid = undefined;
+        var tid = req.param('tid');
+	    var callback = callbacks.saveCallback(req, res);
+
+        saveChildAndAddToParent(ng, ngid, NoteGroup, tid, Track, callback);
+    });
+
+    // Create a note_group with ngid in composition(cid)/track(tid) 
+    app.post('/composition/:cid/track/:tid/notegroup/:ngid', function(req, res, next) {
+	    var ng = NoteGroup.createFromObject(req.body);
+        var cid = req.param('cid');
+        var tid = req.param('tid');
+        var ngid = req.param('ngid');
+	    var callback = callbacks.saveCallback(req, res);
+
+        saveChildAndAddToParent(ng, ngid, NoteGroup, tid, Track, callback);        
     });
 
 }
